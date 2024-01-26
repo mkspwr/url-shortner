3f1115ba2980327c8581b5f5601e41394f8afa4d
Vihaan <lionvihaan@gmail.com>
2024-01-25 19:49:33 -0800

    stoof

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os.path
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +18,14 @@ def about():
 def your_url():
     if request.method == 'POST':
         urls = {}
+
+        if os.path.exists ('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+                if request.form['code'] in urls.keys():
+                   return redirect(url_for('home')) 
+       
         urls[request.form['code']] = {'url':request.form['url']}
         with open('url.json','w') as url_file:
             json.dump(urls, url_file)
